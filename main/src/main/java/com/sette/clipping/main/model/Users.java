47ac99f579f72978fe611012d9dfd79863617fd2
olivera <olivera.bashevska.javen@gmail.com>
2022-07-15 20:25:50 +0200

    old java version set from 11 to 8

--- main/src/main/java/com/sette/clipping/main/model/Users.java
+++ main/src/main/java/com/sette/clipping/main/model/Users.java
@@ -1,5 +1,7 @@
 package com.sette.clipping.main.model;
 
+import jdk.jfr.DataAmount;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
